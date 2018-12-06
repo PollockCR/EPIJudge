96bb046c633d95ca9ef46da355fc17112b89a95e
Catherine <pollockcr@gmail.com>
2018-12-06 11:13:27 -1000

    Finished 11.1: Merge sorted files

--- epi_judge_cpp/sorted_arrays_merge.cc
+++ epi_judge_cpp/sorted_arrays_merge.cc
@@ -1,9 +1,20 @@
 #include <vector>
+#include <queue>
 #include "test_framework/generic_test.h"
 using std::vector;
 vector<int> MergeSortedArrays(const vector<vector<int>>& sorted_arrays) {
-  // TODO - you fill in here.
-  return {};
+  vector<int> final_array;
+  std::priority_queue<int, vector<int>, std::greater<int>> min_heap;
+  for(auto single_array : sorted_arrays){
+    for(auto value : single_array){
+      min_heap.emplace(value);
+    }
+  }
+  while(!min_heap.empty()){
+    final_array.emplace_back(min_heap.top());
+    min_heap.pop();
+  }
+  return final_array;
 }
 
 int main(int argc, char* argv[]) {
