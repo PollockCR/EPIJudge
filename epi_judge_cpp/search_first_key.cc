efae260a6c9dc55cae4271587854f5cb16240a06
Catherine <pollockcr@gmail.com>
2018-12-06 14:17:03 -1000

    Finished 12.1: Search a sorted array for first occurance of k

--- epi_judge_cpp/search_first_key.cc
+++ epi_judge_cpp/search_first_key.cc
@@ -2,9 +2,34 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
+int SearchFirstOfKHelper(const int start, const int end, const vector<int>& A, int k){
+  if(start > end){
+    return -1;
+  }
+  int mid = (start + end) / 2;
+  if(A[mid] == k){
+    /* initial approach: backtrack to first k
+    while(mid >= start && A[mid] == k){
+      mid--;
+    }
+    return mid+1;
+    */
+    // approach from EPI solution: continue binary search_first_key
+    if(start == mid){
+      return mid;
+    }
+    return SearchFirstOfKHelper(start, mid, A, k);
+  }
+  if(A[mid] > k){
+    return SearchFirstOfKHelper(start, mid-1, A, k);
+  }
+  if(A[mid] < k){
+    return SearchFirstOfKHelper(mid+1, end, A, k);
+  }
+}
+
 int SearchFirstOfK(const vector<int>& A, int k) {
-  // TODO - you fill in here.
-  return 0;
+  return SearchFirstOfKHelper(0, A.size()-1, A, k);
 }
 
 int main(int argc, char* argv[]) {
