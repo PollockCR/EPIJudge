77a09f8eecd72784dc82e79c91555e8b5eb95144
Catherine <pollockcr@gmail.com>
2018-12-09 10:05:30 -1000

    Finished 14.1: Compute the intersection of two sorted arrays

--- epi_judge_cpp/intersect_sorted_arrays.cc
+++ epi_judge_cpp/intersect_sorted_arrays.cc
@@ -1,11 +1,32 @@
+/* 14.1 COMPUTE THE INTERSECTION OF TWO SORTED ARRAYS
+Write a program which takes as input two sorted arrays, and returns a new array
+containing elements that are present in both of the input arrays. The input arrays
+may have duplicate entries, but the returned array should be free of duplicates. For
+example, the input is (2,3,3,5,5,6,7,7,8, 12) and (5,5,6,8,8,9, 10, 10), your output
+should be (5, 6, 8).
+*/
 #include <vector>
 #include "test_framework/generic_test.h"
 using std::vector;
 
 vector<int> IntersectTwoSortedArrays(const vector<int>& A,
                                      const vector<int>& B) {
-  // TODO - you fill in here.
-  return {};
+  vector<int> intersect;
+  int iA = 0, iB = 0;
+  while(iA < A.size() && iB < B.size()){
+    if(A[iA] == B[iB]){
+      if(intersect.empty() || intersect.back() != A[iA]){
+        intersect.emplace_back(A[iA]);
+      }
+      iA++;
+      iB++;
+    } else if(A[iA] < B[iB]){
+      iA++;
+    } else {
+      iB++;
+    }
+  }
+  return intersect;
 }
 
 int main(int argc, char* argv[]) {
