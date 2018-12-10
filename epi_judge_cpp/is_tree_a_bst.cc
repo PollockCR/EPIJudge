fbd029aee864d979b619155c1dae033fb9c7806f
Catherine <pollockcr@gmail.com>
2018-12-09 16:27:12 -1000

    Finished 15.1: Test if a binary tree satisfies the BST property

--- epi_judge_cpp/is_tree_a_bst.cc
+++ epi_judge_cpp/is_tree_a_bst.cc
@@ -1,11 +1,26 @@
+/* 15.1 TEST IF A BINARY TREE SATISFIES THE BST PROPERTY
+Write a program that takes as input a binary tree and checks if the tree satisfies the
+BST property.
+*/
 #include <memory>
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
 using std::unique_ptr;
 
+bool AreKeysInRange(const unique_ptr<BinaryTreeNode<int>>& tree, int low, int high){
+  if(tree == NULL){
+    return true;
+  }
+  if(tree->data >= low && tree->data <= high){
+    return AreKeysInRange(tree->left, low, tree->data) &&
+      AreKeysInRange(tree->right, tree->data, high);
+  } else {
+    return false;
+  }
+}
+
 bool IsBinaryTreeBST(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+  return AreKeysInRange(tree, std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
 }
 
 int main(int argc, char* argv[]) {
